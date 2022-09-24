9ad9644c96890a4ac84a0761acc8bc12452b4ac7
JayAslaliya10 <jayaslaliya1503@gmail.com>
2022-09-24 07:16:08 +0530

    fifth commit

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-<<<<<<< HEAD
-
-//jshint esversion:6
-const express = require("express");
+const express=require('express')
+const mongoose=require('mongoose')
+const bcrypt=require('bcryptjs')
 const bodyParser = require("body-parser");
+const jwt=require('jsonwebtoken')
 const ejs = require("ejs");
+const User=require('./models/user')
+const emergencyDesc=require('./models/emergencydesc')
+const app=express()
 
-const mongoose=require("mongoose");
+ACCOUNT_SID="AC7cfae453d585e28499d880d5be57a23a"
+AUTH_TOKEN="8e457010af2e69808c6261dfb64838d2"
+const twilio = require('twilio')(ACCOUNT_SID,AUTH_TOKEN)
 
-
-var twilio=require('twilio');   
-var accountSid = "ACcaca11fc101c73f5fbc854f8bb7d49b1"; // Your Account SID from www.twilio.com/console
-var authToken ="853ae72af4a5b37834bc0022a53c11fc"; 
-const app = express();
+app.use(express.json())
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/dataaDB", {
-    useNewUrlParser:true, 
-});
-const userSchema=new mongoose.Schema({
-    description:{type: String},
-    contact:{type: String },
-    address:{type: String},
-});
+const mongoURI = "mongodb+srv://abc:abc@cluster0.225dbiz.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongoURI,()=>{
+    console.log('connected to DB');
+})
 
-const User=mongoose.model("User", userSchema);
+var numberslist =[]
+
 
 
 app.get("/", function (req, res) {
@@ -50,70 +48,33 @@ app.get("/aboutus",function(req,res){
 app.get("/contact", function (req, res) {
     res.render("contact");
 });
-app.post("/crime", function (req, res) {
-    res.render("crime");
-});
-app.post("/calamity", function (req, res) {
-    res.render("calamity");
-});
-app.get("/signup", function (req, res) {
-    res.render("signup");
-});
-app.post("/fire",function(req,res){
-    res.render("fire");
-    const user = new User({
-        description: req.body.description,
-        contact: req.body.contact,
-        address:req.body.address
-      });
-});
-
-app.post("/health",function(req,res){
-    res.render("health");
-});
-// app.get("/aboutus",function(req,res){
-//     var client=new twilio(accountSid,authToken);
-//     client.messages.create({
-//         body: ",we have a new shelter in your area.Please help us to find stray dogs ",
-//         to:"8104720577",
-//         from:"+18148460647"
-//     },(err,message)=>{
-//         console.log(message.sid);
-//     }
-//     )
-
+// app.post("/crime", function (req, res) {
+//     res.render("crime");
+// });
+// app.post("/calamity", function (req, res) {
+//     res.render("calamity");
+// });
+// app.get("/signup", function (req, res) {
+//     res.render("signup");
+// });
+// app.post("/fire",function(req,res){
+//     res.render("fire");
+//     const user = new User({
+//         description: req.body.description,
+//         contact: req.body.contact,
+//         address:req.body.address
+//       });
 // });
 
+// app.post("/health",function(req,res){
+//     res.render("health");
+// });
 
-app.listen(3000, function () {
-    console.log("server started on port 3000");
-});
-=======
-const express=require('express')
-const mongoose=require('mongoose')
-const bcrypt=require('bcryptjs')
-const jwt=require('jsonwebtoken')
-const User=require('./models/user')
-const emergencyDesc=require('./models/emergencydesc')
-const app=express()
-
-ACCOUNT_SID="AC7cfae453d585e28499d880d5be57a23a"
-AUTH_TOKEN="8e457010af2e69808c6261dfb64838d2"
-const twilio = require('twilio')(ACCOUNT_SID,AUTH_TOKEN)
-
-app.use(express.json())
-
-
-var numberslist =[]
-
-const mongoURI = "mongodb+srv://abc:abc@cluster0.225dbiz.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(mongoURI,()=>{
-    console.log('connected to DB');
-
+app.get('/signup',async(req,res)=>{
+    res.render("signup")
 })
 
-
-app.post('/register',async(req, res)=>{
+app.post('/signup',async(req, res)=>{
     let success = false;
   
       try{
@@ -127,11 +88,9 @@ app.post('/register',async(req, res)=>{
         const secPass = await bcrypt.hash(req.body.password, salt)
   
         user = await User.create({
-          name : req.body.name,
-          phone : req.body.phone,
+          name : req.body.txt,
           email : req.body.email,
-          password : secPass,
-          address: req.body.address,
+          password : req.body.pswd,
         })
 
         numberslist.push(String(req.body.phone))
@@ -150,6 +109,8 @@ app.post('/register',async(req, res)=>{
         console.log(err);
         res.json({status : 'error', error : err})
       }
+
+      res.render("signup")
     })
 
 
@@ -188,7 +149,8 @@ app.post('/login',async(req,res)=>{
 
 
 app.post("/fire",(req,res)=>{
-    const numbers=["+917021437655","+918104720577"]
+    sendnotif=()=>{
+        const numbers=["+917021437655","+918104720577"]
     Promise.all(
         numbers.map(number=>{
             twilio.messages.create({
@@ -205,68 +167,137 @@ app.post("/fire",(req,res)=>{
     })
 
     desc=emergencyDesc.create({
-        description:req.body.description,
-        contact
+        description:req.body.Description,
+        contact:req.body.Contact,
+        address:req.body.Address
     })
+    }
 
-    // twilio.messages.create({
-    //     body: `There is a fire outbreak ${req.body.inputBox}. Be careful and be safe`,
-    //     to:"+917021437655",
-    //     from:"+15095162458"
-    // }).then(msg=>{console.log('message sent');}).catch(err=>{console.log(err);})
-    
-    // twilio.messages.create({
-    //     body: `There is a fire outbreak ${req.body.inputBox}. Be careful and be safe`,
-    //     to:"+918104720577",
-    //     from:"+15095162458"
-    // }).then(msg=>{console.log('message sent');}).catch(err=>{console.log(err);})
+    res.render("fire")
+
 })
 
 
 
 app.post("/crime",(req,res)=>{
 
-    twilio.messages.create({
-        body: `A crime has occured at ${req.body.inputBox}. Beware and be safe`,
-        to:"+917021437655",
-        from:"+15095162458"
-    }).then(msg=>{
+    // twilio.messages.create({
+    //     body: `A crime has occured at ${req.body.inputBox}. Beware and be safe`,
+    //     to:"+917021437655",
+    //     from:"+15095162458"
+    // }).then(msg=>{
+    //     console.log('message sent successfully');
+    // }).catch(err=>{
+    //     console.log(err);
+    // })
+
+    sendnotif=()=>{
+        const numbers=["+917021437655","+918104720577"]
+    Promise.all(
+        numbers.map(number=>{
+            twilio.messages.create({
+                body: `There is a crime at ${req.body.inputBox}. Be careful and be safe`,
+                to:number,
+                from:"+15095162458"
+            })
+            // console.log(number);
+        })
+    ).then(msg=>{
         console.log('message sent successfully');
     }).catch(err=>{
         console.log(err);
     })
+
+    desc=emergencyDesc.create({
+        description:req.body.Description,
+        contact:req.body.Contact,
+        address:req.body.Address
+    })
+    }
+
+    res.render("crime")
 })
 
 
 app.post("/health",(req,res)=>{
 
-    twilio.messages.create({
-        body: `There is a health emergency at ${req.body.inputBox}. Please help the respective in any manner possible`,
-        to:"+917021437655",
-        from:"+15095162458"
-    }).then(msg=>{
+    // twilio.messages.create({
+    //     body: `There is a health emergency at ${req.body.inputBox}. Please help the respective in any manner possible`,
+    //     to:"+917021437655",
+    //     from:"+15095162458"
+    // }).then(msg=>{
+    //     console.log('message sent successfully');
+    // }).catch(err=>{
+    //     console.log(err);
+    // })
+    sendnotif=()=>{
+        const numbers=["+917021437655","+918104720577"]
+    Promise.all(
+        numbers.map(number=>{
+            twilio.messages.create({
+                body: `There is a health emergency ${req.body.inputBox}. Be careful and be safe`,
+                to:number,
+                from:"+15095162458"
+            })
+            // console.log(number);
+        })
+    ).then(msg=>{
         console.log('message sent successfully');
     }).catch(err=>{
         console.log(err);
     })
+
+    desc=emergencyDesc.create({
+        description:req.body.Description,
+        contact:req.body.Contact,
+        address:req.body.Address
+    })
+    }
+
+    res.render("health")
 })
 
 
 app.post("/calamity",(req,res)=>{
 
-    twilio.messages.create({
-        body: `There has been a calamity at ${req.body.inputBox}. Please help the people of area in any way possible`,
-        to:"+917021437655",
-        from:"+15095162458"
-    }).then(msg=>{
+    // twilio.messages.create({
+    //     body: `There has been a calamity at ${req.body.inputBox}. Please help the people of area in any way possible`,
+    //     to:"+917021437655",
+    //     from:"+15095162458"
+    // }).then(msg=>{
+    //     console.log('message sent successfully');
+    // }).catch(err=>{
+    //     console.log(err);
+    // })
+
+    sendnotif=()=>{
+        const numbers=["+917021437655","+918104720577"]
+    Promise.all(
+        numbers.map(number=>{
+            twilio.messages.create({
+                body: `There is a calamity occured at ${req.body.inputBox}. Be careful and be safe`,
+                to:number,
+                from:"+15095162458"
+            })
+            // console.log(number);
+        })
+    ).then(msg=>{
         console.log('message sent successfully');
     }).catch(err=>{
         console.log(err);
     })
+
+    desc=emergencyDesc.create({
+        description:req.body.Description,
+        contact:req.body.Contact,
+        address:req.body.Address
+    })
+    }
+
+    res.render("calamity")
 })
 
 
 app.listen(3000,()=>{
     console.log('listening to port 3000');
 })
->>>>>>> 245d9593c0f4aa58009754ba5e122b1e47d56931
